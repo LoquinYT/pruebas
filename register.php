097eabae1979d5ef61f5bb44955a39f4cdd09ceb
LoquinYT <74012790+LoquinYT@users.noreply.github.com>
2020-11-06 09:12:07 -0300

    Add files via upload

---
+++ register.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang=es>
+  <head>
+    <meta charset="UTF-8">
+    <title>Ruffle | Register</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+  <imput type="text" placeholder="Correo electrónico">
+
+
+
+  </body>
